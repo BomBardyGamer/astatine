345377e73bb2acc0e96a47bd83f8bcd741d67cc4
Callum Seabrook <callum.seabrook@prevarinite.com>
2026-01-05 20:06:19 +0000

    feat: attribute parsing: record

--- src/parser/classfile/attribute/record.rs
+++ src/parser/classfile/attribute/record.rs
@@ -17,3 +17,33 @@ mod _attr_name {
 
     impl_attr_name!(Record, RECORD);
 }
+
+mod _parse {
+    use crate::buf_read_named_type_vec;
+    use crate::parser::{BinaryReader, Parse, ParserError};
+    use super::*;
+
+    impl Parse<Record> for Record {
+        fn parse(buf: &mut BinaryReader) -> Result<Record, ParserError> {
+            buf_read_named_type_vec!(Component, components, buf,
+                "record - components", "record - components - idx {}");
+            Ok(Record { components })
+        }
+    }
+
+    impl Parse<Component> for Component {
+        fn parse(buf: &mut BinaryReader) -> Result<Component, ParserError> {
+            buf.check_bytes(2 + 2, "name index, descriptor index")?;
+
+            // SAFETY: Guaranteed by check_bytes
+            let name_index = unsafe { buf.unsafe_read_u16() };
+            let descriptor_index = unsafe { buf.unsafe_read_u16() };
+
+            Ok(Component {
+                name_index,
+                descriptor_index,
+                attributes: vec![], // TODO: Attributes
+            })
+        }
+    }
+}
